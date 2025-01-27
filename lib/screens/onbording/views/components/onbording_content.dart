9a80091584ac466e30b1b7f21fc003f20294ef2f
Qharny <cannycc1020@gmail.com>
2025-01-27 23:21:19 +0000

    added flutter updated

--- lib/screens/onbording/views/components/onbording_content.dart
+++ lib/screens/onbording/views/components/onbording_content.dart
@@ -36,7 +36,7 @@ class OnbordingContent extends StatelessWidget {
         if (!isTextOnTop) const Spacer(),
         if (!isTextOnTop)
           const OnbordTitleDescription(
-            title: "Find the item you’ve \nbeen looking for",
+            title: "Find Quality Building \nMaterials Easily",
             description:
                 "Here you’ll see rich varieties of goods, carefully classified for seamless browsing experience.",
           ),
